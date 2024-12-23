c1c03acb2b7241231ffacf606902a0dfece1200c
Min Jiwon <peace.cheejecake@gmail.com>
2024-12-23 22:19:58 +0900

    feat: default values on App.vue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { processArguments } from "./utils/process.js";
-import logger from "./utils/logger.js";
+import { processArguments } from './utils/process.js';
+import logger from './utils/logger.js';
 import App from './App.js';
 
 const app = new App();
@@ -7,11 +7,11 @@ const args = processArguments();
 
 logger.info(args);
 
-app.init({ 
-	market: args.market ?? 'KRW-IOTA', 
-	intervalBuy: args.interval ? Number(args.interval) : 5, 
-	intervalSell: args.interval ? Number(args.interval) : 5,
-	thresholdBuy: args['t-buy'] ? Number(args['t-buy']) : undefined,
-	thresholdSellWin: args['t-sell-win'] ? Number(args['t-sell-win']) : undefined,
-	thresholdSellLose: args['t-sell-lose'] ? Number(args['t-sell-lose']) : undefined,
+app.init({
+  market: args.market ?? 'KRW-IOTA',
+  intervalBuy: args.interval ? Number(args.interval) : 5,
+  intervalSell: args.interval ? Number(args.interval) : 5,
+  thresholdBuy: args['t-buy'] ? Number(args['t-buy']) : -0.002,
+  thresholdSellWin: args['t-sell-win'] ? Number(args['t-sell-win']) : 0.002,
+  thresholdSellLose: args['t-sell-lose'] ? Number(args['t-sell-lose']) : -0.02,
 });
